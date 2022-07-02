f0e75c11877d9bd5a34cd0957eb193a68f779969
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-07-02 23:30:14 +0300

    style:add backgound colour & inputs to the login page

--- Front-end/Tracker/src/app/app.module.ts
+++ Front-end/Tracker/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './main/navbar/navbar.component';
 //import { LoginComponent } from './users/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
