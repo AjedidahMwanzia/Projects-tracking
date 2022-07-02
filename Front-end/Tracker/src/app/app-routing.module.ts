25c4a0148b913080dcff9c5ccbc0aae8d8989efd
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-07-03 00:55:53 +0300

    ft:add profile image to user profile page.

--- Front-end/Tracker/src/app/app-routing.module.ts
+++ Front-end/Tracker/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './users/login/login.component';
+import { UserProfileComponent } from './users/user-profile/user-profile.component';
 
 const routes: Routes = [
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'my-profile',component:UserProfileComponent}
 ];
 
 @NgModule({
--- Front-end/Tracker/src/app/app.module.ts
+++ Front-end/Tracker/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './main/navbar/navbar.component';
+import { UserProfileComponent } from './users/user-profile/user-profile.component';
 //import { LoginComponent } from './users/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    NavbarComponent
+    NavbarComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
