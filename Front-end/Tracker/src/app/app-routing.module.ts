b25980057cd9f3a34d98bb773f7cde0787208499
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-07-02 21:14:07 +0300

    ft:add routing for the login page

--- Front-end/Tracker/src/app/app-routing.module.ts
+++ Front-end/Tracker/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './users/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+// array of all routing components
+export const routingComponents = [LoginComponent]
