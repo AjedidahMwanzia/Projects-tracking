f66931fe2ed5e3f0fd68164c9db4dd37a0529493
kleitich <kevleitich@gmail.com>
2022-07-03 20:39:57 +0300

    adding profile serializers

--- Back-end/Tracker/settings.py
+++ Back-end/Tracker/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'cloudinary',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- Back-end/app/views.py
+++ Back-end/app/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from app.forms import UserRegistrationForm
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Profile
+from .serializers import ProfileSerializer
 
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Your account has been created. You can log in now!')    
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'auth/register.html', context)
+
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profile = Profile.objects.all()
+        serializers = ProfileSerializer(all_profile, many=True)
+        return Response(serializers.data)
