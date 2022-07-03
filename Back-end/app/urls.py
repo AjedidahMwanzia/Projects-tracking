0fe73323782f58970ae5713eacdf4d2184ee6f11
kleitich <kevleitich@gmail.com>
2022-07-03 21:33:39 +0300

    adding handling of post request and User api

--- Back-end/app/urls.py
+++ Back-end/app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('register/',views.register_user,name = 'register'),
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='auth/login.html'), name='logout'),
-    path('api/profile/', views.ProfileList.as_view())
+    path('api/profile/', views.ProfileList.as_view()),
+    path('api/user/', views.UserList.as_view()),
 ]
--- Back-end/app/views.py
+++ Back-end/app/views.py
@@ -4,8 +4,9 @@ from app.forms import UserRegistrationForm
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import  Profile
-from .serializers import ProfileSerializer
-
+from .serializers import ProfileSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import status
 # Create your views here.
 def home(request):
     return render(request,'index.html')
@@ -30,3 +31,21 @@ class ProfileList(APIView):
         all_profile = Profile.objects.all()
         serializers = ProfileSerializer(all_profile, many=True)
         return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserList(APIView):
+    def get(self, request, format=None):
+        all_user = User.objects.all()
+        serializers = UserSerializer(all_user, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = UserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
