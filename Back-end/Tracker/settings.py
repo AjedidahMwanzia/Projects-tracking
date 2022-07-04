3e77f3aac8f918a16aea15f19bfe31b020e71b86
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-07-04 14:58:29 +0300

    Add djangocors headers

--- Back-end/Tracker/settings.py
+++ Back-end/Tracker/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'cloudinary',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,3 +135,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+   
+    "http://localhost:4200",
+    "http://127.0.0.1:8000",
+]
