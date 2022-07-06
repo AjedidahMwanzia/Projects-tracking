6ead9df21ab53481a4bf45f1ef01fcd938452598
kleitich <kevleitich@gmail.com>
2022-07-06 15:26:04 +0300

    model user in models

--- Back-end/app/models.py
+++ Back-end/app/models.py
@@ -8,7 +8,14 @@ from cloudinary.models import CloudinaryField
 
 
 
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=255,unique=True)
+    username= models.CharField(max_length=255,unique=True)
 
+    # USERNAME_FIELD='email'
+    # REQUIRED_FIELDS=[]
 
 
 
